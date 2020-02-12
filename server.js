b5643b1db040bb7aafb14693b9efc3475d5446b9
Tahsib <tahsib7@gmail.com>
2020-02-12 23:58:29 +0600

    chat feature added

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 
 var messages = [
-    {name: "Linda", message: "Hi there!"},
-    {name: "ryu", message: "Hello peeps!"}
+    
 ]
+
 app.get('/messages',(req,res)=>{
     res.send(messages)
 })
 
-var server = app.listen(3000,()=>{
+app.post("/messages",(req,res)=>{
+    messages.push(req.body);
+    io.emit('message',req.body)
+    res.sendStatus(200);
+    
+})
+
+io.on('connection', (socket)=>{
+    console.log("User connected")
+})
+
+var server = http.listen(3000,()=>{
     console.log("Server is listening on port", server.address().port)
 })
