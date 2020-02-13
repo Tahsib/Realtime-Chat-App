28094e80ca5d97083598d47e4fe59b94dc21e436
Tahsib <tahsib7@gmail.com>
2020-02-13 12:02:31 +0600

    Database added

--- server.js
+++ server.js
@@ -3,29 +3,45 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+require('dotenv').config()
+var mongoose = require('mongoose')
 
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
-var messages = [
-    
-]
+var dbUrl='mongodb://' + process.env.USER + ':' + process.env.PASS + '@ds239648.mlab.com:39648/chat-app'
+
+var Message = mongoose.model('Message',{
+    name: String,
+    message: String
+})
+
 
 app.get('/messages',(req,res)=>{
-    res.send(messages)
+    Message.find({},(err,messages)=>{
+        res.send(messages)
+    })
 })
 
 app.post("/messages",(req,res)=>{
-    messages.push(req.body);
-    io.emit('message',req.body)
-    res.sendStatus(200);
-    
+    var message = new Message(req.body)
+    message.save((err)=>{
+        if(err){
+            sendStatus(500)
+        }
+        io.emit('message',req.body)
+        res.sendStatus(200);
+    })
 })
 
 io.on('connection', (socket)=>{
     console.log("User connected")
+})
+
+mongoose.connect(dbUrl,{useNewUrlParser: true,useUnifiedTopology: true},(err)=>{
+    console.log("MongoDB connected "+err)
 })
 
 var server = http.listen(3000,()=>{
